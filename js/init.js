d4f0b3073acd520da82920ed73af36ab186b5837
VeronicaVazqu <93964853+VeronicaVazqu@users.noreply.github.com>
2021-11-10 22:44:01 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDOYMpp68_udyaxhGI6xbtyF0n2HURHnP4",
-  authDomain: "fbauten1001.firebaseapp.com",
-  projectId: "fbauten1001",
-  storageBucket: "fbauten1001.appspot.com",
-  messagingSenderId: "717078882003",
-  appId: "1:717078882003:web:c4c3e1e5a917385cced626",
-  measurementId: "G-3QL4D2NXDN"
+  apiKey: "AIzaSyDZigeSAmRgPEH3bXGO2hKh7pmuIEFz9z8",
+  authDomain: "chat-70a47.firebaseapp.com",
+  projectId: "chat-70a47",
+  storageBucket: "chat-70a47.appspot.com",
+  messagingSenderId: "1006265947485",
+  appId: "1:1006265947485:web:c0a1aa7ed0d4b7919a84b4",
+  measurementId: "G-0HKV9L3K56"
     });
